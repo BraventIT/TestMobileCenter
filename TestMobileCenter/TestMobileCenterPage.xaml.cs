f596ff027694f005e7749f77dcc15bee977e6ba5
Daniel Cardeña Pulido <dcardena@bravent.net>
2017-08-03 09:27:55 +0200

    first upload

---
+++ TestMobileCenter/TestMobileCenterPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace TestMobileCenter
+{
+    public partial class TestMobileCenterPage : ContentPage
+    {
+        public TestMobileCenterPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
